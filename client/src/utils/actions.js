7b7eddbb938285d1fe1a1d081019f18c179a69ef
Duke Nukem <garrettduke87@gmail.com>
2021-01-17 11:38:47 -0700

    2.1.4

---
+++ client/src/utils/actions.js
@@ -0,0 +1,5 @@
+// these actions define how three parts of our state will be maintained and updated
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+
